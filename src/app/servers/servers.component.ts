ac3cf8f8630acf7ff4c1ae833852cb9925c2f8ac
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2024-03-07 16:18:05 +0000

    Add styling to components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // selector: [app-servers],
+  // selector: '.app-servers',
+  selector: 'app-servers', //best to use for components
+  template: '<app-server></app-server><app-server></app-server>',
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
