5a7efba49042cf14b76fec7ca21614457753a225
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2024-03-11 17:31:41 +0000

    Add property and event binding to servers app

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,10 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   // selector: [app-servers],
   // selector: '.app-servers',
-  selector: 'app-servers', //best to use for components
-  template: '<app-server></app-server><app-server></app-server>',
-  styleUrl: './servers.component.css'
+  selector: 'app-servers', 
+  //best to use for components
+  // template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css' 
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!";
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer =  true;
+    },2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Sever was created"
+  }
 }
